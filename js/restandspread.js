685a8289bdc1ec5e324074bbef56ae8daeb8fed5
Abinash <abinash261997@gmail.com>
2022-07-15 17:27:58 +0530

    sperad operation in array

--- js/restandspread.js
+++ js/restandspread.js
@@ -1,9 +1,17 @@
-// // add number using rest operator
+// // // add number using rest operator
 
-function addNumbers(a, b, c, ...others) {
-    console.log(others);
-    // return a + b + c;
-    // result sumof(others)
+// function addNumbers(a, b, c, ...others) {
+//     console.log(others);
+//     // return a + b + c;
+//     // result sumof(others)
+// }
+// const result = addNumbers(1, 2, 3, 4, 5);
+// console.log(result);
+
+// spread operator
+var names = ["Abinash", "kartik", "Alok", "Sanjay"];
+function getNames(n1, n2, n3, n4) {
+    console.log(n1, n2, n3, n4);
 }
-const result = addNumbers(1, 2, 3, 4, 5);
-console.log(result);
+getNames(names[0], names[1], names[2]);
+getNames(...names);
